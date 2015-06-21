199cbc8f0656384acea2e85582a53b5fa774d645
Anton Gorchakov <gorchaqw@gmail.com>
2015-06-21 18:35:33 +0000

    fast fix

---
+++ database/migrations/2015_06_19_223020_create_galleries_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGalleriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('galleries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('title');
+            $table->text('url_img');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('galleries');
+    }
+}
