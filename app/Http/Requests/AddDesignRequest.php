afaba0b76f7b39cc55b9391516823a8fd0526276
Anton Gorchakov <gorchaqw@gmail.com>
2015-06-22 21:09:57 +0000

    hotfix

---
+++ app/Http/Requests/AddDesignRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class AddDesignRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title'         =>'min:2',
+            'url_img'       =>'image',
+            'description'   =>'min:2',
+            'catalog_id'    =>'integer'
+        ];
+    }
+}
---
+++ app/Http/Requests/AddPictureRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class AddPictureRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title'         =>'min:2',
+            'url_img'       =>'min:2',
+            'description'   =>'min:2'
+        ];
+    }
+}
